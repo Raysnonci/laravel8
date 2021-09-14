78a4b8762e5583f416c8d15a551115bf3ccaaa83
Raynaldi <raysnonci@gmail.com>
2021-09-15 06:01:38 +0700

    [DR2] Blade Escape Character

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,7 +5,7 @@
         <article>
             <h2><a href="/blog/{{ $post->id }}">{{ $post->title }}</a></h2>
             {{-- <h5>{{ $post['author'] }}</h5> --}}
-            <p>{{ $post->body }}</p>
+            {!! $post->body !!}
         </article>
     </div>
     <a href="/blog">Back to the blog</a>
